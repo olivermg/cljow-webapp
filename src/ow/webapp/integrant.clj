83682b658df4839f6e1700b539a7c078b51141b8
Oliver Wegner <void1976@gmail.com>
2019-01-14 16:00:41 +0100

    working on integrant

--- src/ow/webapp/integrant.clj
+++ src/ow/webapp/integrant.clj
@@ -1,9 +1,14 @@
 (ns ow.webapp.integrant
-  (:require [integrant.core :as ic]
+  (:require [integrant.core :as ig]
             [ow.webapp :as wa])
   (:import [ow.webapp Webapp]))
 
-(defmethod ic/init-key :ow.webapp/integrant [_ opts]
+(defmethod ig/init-key :ow.webapp/integrant [_ opts]
   (if-not (instance? Webapp opts)
-    (wa/map->Webapp opts)
+    (-> (wa/map->Webapp opts)
+        (wa/start))
     opts))
+
+(defmethod ig/halt-key! :ow.webapp/integrant [_ this]
+  (when (instance? Webapp this)
+    (wa/stop this)))
