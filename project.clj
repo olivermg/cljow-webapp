bcb6cc7c29bda2178bf1c27f55dad5098479d616
Oliver Wegner <void1976@gmail.com>
2019-01-18 15:16:39 +0100

    adding clojure logging dependency

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [cljow-app "0.1.0-SNAPSHOT"]
                  [http-kit "2.3.0"]
                  [org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.logging "0.4.1"]
                  [ring/ring-defaults "0.3.2"]
                  [bidi "2.1.5"]]
 
