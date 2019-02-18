d2f5bd14fe94cb164519483e2e0e92a45965a37f
Oliver Wegner <void1976@gmail.com>
2019-02-18 19:28:37 +0100

    use new Lifecycle in webapp

--- src/ow/webapp.clj
+++ src/ow/webapp.clj
@@ -1,13 +1,15 @@
 (ns ow.webapp
   (:require [bidi.bidi :as b]
+            [clojure.tools.logging :as log]
             [org.httpkit.server :as hk]
+            [ow.app.msgcomponent :as owmc]
             [ow.app.lifecycle :as owl]))
 
-(defn- app-handler [{:keys [routes resources] :as this} req]
+(defn- app-handler [{:keys [routes resources] :as this} parent req]
   (let [{:keys [handler]} (some-> (b/match-route routes (:uri req))
                                   (update :handler #(get resources %)))]
     (if handler
-      (handler req)
+      (handler (assoc req ::this parent))
       {:status 404
        :body "resource not found"})))
 
@@ -16,24 +18,28 @@
 
   owl/Lifecycle
 
-  (start [this]
+  (start* [this parent]
     (if-not server
-      (let [server (hk/run-server
-                    ((or middleware identity) (partial app-handler this))
-                    (merge {:port 8080
-                            :worker-name-prefix "httpkit-worker-"}
-                           httpkit-options))]
-        (assoc this :server server))
+      (do (log/info "Starting ow.webapp.Webapp")
+          (let [server (hk/run-server
+                        ((or middleware identity) (partial app-handler this parent))
+                        (merge {:port 8080
+                                :worker-name-prefix "httpkit-worker-"}
+                               httpkit-options))]
+            (assoc this :server server)))
       this))
 
-  (stop [this]
+  (stop* [this parent]
     (when server
+      (log/info "Stopping ow.webapp.Webapp")
       (server))
-    (assoc this
-           :server nil)))
+    (assoc this :server nil)))
 
 (defn webapp [routes resources & {:keys [middleware httpkit-options]}]
   (map->Webapp {:routes routes
                 :resources resources
                 :middleware middleware
                 :httpkit-options httpkit-options}))
+
+(defn webappify [parent & args]
+  (assoc parent ::this (apply webapp args)))
