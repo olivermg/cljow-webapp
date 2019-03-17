ce9b31c22f8fe1657d6e3357764df5762f6e3781
Oliver Wegner <void1976@gmail.com>
2019-03-17 14:02:47 +0100

    removing obsolete code

--- src/ow/webapp/async.clj
+++ src/ow/webapp/async.clj
@@ -3,8 +3,6 @@
             [clojure.tools.logging :as log]
             [org.httpkit.server :as hk]
             [ow.app.lifecycle :as owl]
-            #_[ow.app.messaging :as owm]
-            #_[ow.app.messaging.component :as owc]
             [ow.app.request-response-component :as owrrc]))
 
 (defn- request-handler [middleware-instance this http-req]
@@ -20,59 +18,11 @@
                               (owrrc/get-data))]
         (hk/send! ch (middleware-instance (assoc http-req ::captured-response response-data)))))))
 
-#_(defn- response-handler [middleware-instance pending-channels messaging-component msg]
-  (let [flow-id (owm/get-flow-id msg)
-        response (owm/get-data msg)]
-    (when-let [[ch orig-req] (get @pending-channels flow-id)]
-      (when (hk/open? ch)
-        (hk/send! ch (middleware-instance (assoc orig-req ::captured-response response))))
-      (swap! pending-channels #(dissoc % flow-id)))))
-
 (defn- capturing-handler [{:keys [::captured-request ::captured-response] :as req}]
   (when captured-request
     (reset! captured-request (dissoc req ::captured-request ::captured-response)))
   (when captured-response
     captured-response))
-
-#_(defn- retrieving-request-handler [{:keys [::captured-response] :as req}]
-  captured-response)
-
-#_(defrecord Webapp [messaging-component middleware httpkit-options pending-channels
-                   server]
-
-  owl/Lifecycle
-
-  (start [this]
-    (if-not server
-      (do (log/info "Starting ow.webapp.async.Webapp")
-          (let [server (hk/run-server (partial request-handler (middleware storing-request-handler) this)
-                                      httpkit-options)]
-            (assoc this
-                   :server server
-                   :messaging-component (owl/start messaging-component))))
-      this))
-
-  (stop [this]
-    (when server
-      (log/info "Stopping ow.webapp.async.Webapp")
-      (server))
-    (assoc this
-           :server nil
-           :messaging-component (owl/stop messaging-component))))
-
-#_(defn webapp [in-ch out-ch & {:keys [middleware httpkit-options]}]
-  (let [middleware (or middleware identity)
-        pending-channels (atom {})
-        partial-handler (partial response-handler (middleware retrieving-request-handler) pending-channels)
-        mc (owc/component "webapp-async" in-ch out-ch :http/response partial-handler)]
-    (map->Webapp {:messaging-component mc
-                  :middleware middleware
-                  :httpkit-options (merge {:port 8080
-                                           :worker-name-prefix "async-webapp-worker-"}
-                                          httpkit-options)
-                  :pending-channels pending-channels})))
-
-
 
 (defn init [this name request-ch response-ch & {:keys [middleware httpkit-options]}]
   (let [middleware (or middleware identity)]
