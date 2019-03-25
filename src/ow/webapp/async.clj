725a1f12d800d0277d82eb84491e359cdcc6a8a8
Oliver Wegner <void1976@gmail.com>
2019-03-25 22:44:58 +0100

    working on async webapp

--- src/ow/webapp/async.clj
+++ src/ow/webapp/async.clj
@@ -5,16 +5,15 @@
             [ow.lifecycle :as owl]
             [ow.comm :as owc]))
 
-(defn- request-handler [middleware-instance this http-req]
+(defn- request-handler [middleware-instance {:keys [::out-ch] :as this} http-req]
   (hk/with-channel http-req ch
     (hk/on-close ch (fn [status]
-                      (log/debug "channel closed with status" status)))
+                      (log/trace "channel closed with status" status)))
     (future
       (let [http-req-after-middlewares (atom http-req)
             _ (middleware-instance (assoc http-req ::captured-request http-req-after-middlewares))
-            response-data (-> (owrrc/request this :http/request @http-req-after-middlewares)
-                              (owrrc/wait-for-response))]
-        (hk/send! ch (middleware-instance (assoc http-req ::captured-response response-data)))))))
+            response (owc/request out-ch @http-req-after-middlewares)]
+        (hk/send! ch (middleware-instance (assoc http-req ::captured-response response)))))))
 
 (defn- capturing-handler [{:keys [::captured-request ::captured-response] :as req}]
   (when captured-request
@@ -23,30 +22,23 @@
     captured-response))
 
 (defn construct [out-ch & {:keys [middleware httpkit-options]}]
-  (assoc this
-         ::out-ch out-ch
-         ::middleware (or middleware identity)
-         ::httpkit-options (merge {:port 8080
-                                   :worker-name-prefix "async-webapp-worker-"}
-                                  httpkit-options)))
+  (owl/construct ::webapp {::out-ch out-ch
+                           ::middleware (or middleware identity)
+                           ::httpkit-options (merge {:port 8080
+                                                     :worker-name-prefix "async-webapp-worker-"}
+                                                    httpkit-options)}))
 
-(defmethod owl/start [{:keys [::middleware ::httpkit-options ::server] :as this}]
+(defmethod owl/start ::webapp [{:keys [::middleware ::httpkit-options ::server] :as this}]
   (if-not server
-    (do (log/info "Starting ow.webapp.async.Webapp")
-        (let [this   (owrrc/start-requester this)
-              server (hk/run-server (partial request-handler (middleware capturing-handler) this)
-                                    httpkit-options)]
-          (assoc this ::runtime {:server server})))
+    (let [server (hk/run-server (partial request-handler (middleware capturing-handler) this)
+                                httpkit-options)]
+      (assoc this ::server server))
     this))
 
-(defn stop [{{:keys [server]} ::runtime
-             :as this}]
+(defmethod owl/stop ::webapp [{:keys [::server] :as this}]
   (when server
-    (log/info "Stopping ow.webapp.async.Webapp")
     (server))
-  (-> this
-      (assoc ::runtime {})
-      owrrc/stop-requester))
+  (assoc this ::server nil))
 
 
 
