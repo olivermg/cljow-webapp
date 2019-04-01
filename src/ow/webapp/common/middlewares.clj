2d0346acac78b52a86930d3b7ca718bf6b4c872f
Oliver Wegner <void1976@gmail.com>
2019-04-01 13:25:35 +0200

    working on middlewares

--- src/ow/webapp/common/middlewares.clj
+++ src/ow/webapp/common/middlewares.clj
@@ -5,22 +5,41 @@
             [ring.middleware.defaults :as rmd]
             [ring.middleware.gzip :as rmg]))
 
-(defn make-middleware [handler & {:keys [api?
-                                         test-environment?
-                                         authentication-backend]}]
-  (let [defaults (if api?
-                   (if-not test-environment?
-                     rmd/secure-api-defaults
-                     rmd/api-defaults)
-                   (if-not test-environment?
-                     rmd/secure-site-defaults
-                     rmd/site-defaults))
+(defn make-middleware [& {:keys [api?
+                                 test-environment?
+                                 authentication-backend]}]
+
+  (let [defaults   (if api?
+                     (if-not test-environment?
+                       rmd/secure-api-defaults
+                       rmd/api-defaults)
+                     (if-not test-environment?
+                       rmd/secure-site-defaults
+                       rmd/site-defaults))
         wrap-auth  (if authentication-backend
                      (fn [handler]
                        (wrap-authentication handler authentication-backend))
                      identity)]
-    (-> handler
-        (mm/wrap-format)
-        (rmd/wrap-defaults defaults)
-        (wrap-auth)
-        (rmg/wrap-gzip))))
+
+    (fn ow.webapp.common.middleware [handler]
+      (-> handler
+          (mm/wrap-format)
+          (rmd/wrap-defaults defaults)
+          (wrap-auth)
+          (rmg/wrap-gzip)))))
+
+
+
+#_(let [mw (make-middleware :test-environment? true
+                          :api? true
+                          :authentication-backend (bbe/basic {:realm "foobar"
+                                                              :authfn (fn [request authdata]
+                                                                        (:username authdata))}))
+      h  (mw (fn [req]
+               (println "REQ" req)
+               {:status 201
+                :body {:foo "bar" :username (:identity req)}}))]
+  (-> (h {:uri "/foo"
+          :request-method :get
+          :headers {"authorization" "Basic Zm9vOmJhcg=="}})
+      (update :body slurp)))
