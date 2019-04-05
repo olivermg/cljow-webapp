d4b66fa34aa9f9cd06d1dcb44441e5f2683160ac
Oliver Wegner <void1976@gmail.com>
2019-04-05 14:18:59 +0200

    improving logging in ow.webapp.async

--- src/ow/webapp/async.clj
+++ src/ow/webapp/async.clj
@@ -6,16 +6,27 @@
             [ow.lifecycle :as owl]))
 
 (defn- request-handler [middleware-instance {:keys [::out-ch] :as this} http-req]
-  (hk/with-channel http-req ch
-    (log/trace "received http request" http-req)
-    (hk/on-close ch (fn [status]
-                      (log/trace "channel closed with status" status)))
-    (future
-      (let [http-req-after-middlewares (atom http-req)
-            _ (middleware-instance (assoc http-req ::captured-request http-req-after-middlewares))
-            response (owc/request out-ch @http-req-after-middlewares)]
-        (log/trace "sending http response" response)
-        (hk/send! ch (middleware-instance (assoc http-req ::captured-response response)))))))
+  (letfn [(handle-exception [req e]
+            (log/debug "FAILED to process http request"
+                       {:request req
+                        :error   e})
+            (throw e))]
+
+    (hk/with-channel http-req ch
+      (log/trace "received http request" http-req)
+      (hk/on-close ch (fn [status]
+                        (log/trace "channel closed with status" status)))
+      (future
+        (try
+          (let [http-req-after-middlewares (atom http-req)
+                _ (middleware-instance (assoc http-req ::captured-request http-req-after-middlewares))
+                response (owc/request out-ch @http-req-after-middlewares)]
+            (log/trace "sending http response" response)
+            (hk/send! ch (middleware-instance (assoc http-req ::captured-response response))))
+          (catch Exception e
+            (handle-exception http-req e))
+          (catch Error e
+            (handle-exception http-req e)))))))
 
 (defn- capturing-handler [{:keys [::captured-request ::captured-response] :as req}]
   (when captured-request
